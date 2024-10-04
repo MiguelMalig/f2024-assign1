64fb935dc22de9a798281f9c963d651926b81fbc
Varainium <rayjustinn@gmail.com>
2024-10-04 17:48:33 -0600

    Established prepared statements inside db_conn file.

--- data/db_conn.php
+++ data/db_conn.php
@@ -12,14 +12,39 @@
         echo "Connection error";
         echo $e -> getMessage();
     }
-    function getData($sql); {
+
+
+    function getData($sql, $parameters = null); {
         try {
             global $pdo;
-            $result = $pdo => query($sql);
-            $data = $result = fetchAll(FETCH_ASSOC);
-            return $data;
+
+            // if no parameters are passed, return data from sql statement.
+            if ($parameters === null) {
+                $result = $pdo -> query($sql);
+                return $result -> fetchAll(FETCH_ASSOC);
+            }
+            else { // if there is parameters passed, enter else statement.
+
+                // if its not an array, make it an array, which is safe to say that if it hits this if function, then its a single parameter..
+                if (!is_array($parameters)) { 
+                    $parameters = [$parameters];
+                }
+                // if there is a passed parameter or more, go through the parameter array and put it inside a sql statement.
+                if (count($parameters) > 0) {
+                    $statement = $pdo -> prepare($sql);
+                    for ($i = 0; $i < count($parameters); $i++) {
+                        $statement -> bindValue($i+1, $parameters[$i]);
+                    }
+                    $statement => execute();
+                    return $statement -> fetchAll(FETCH_ASSOC);
+                }
+            }
+            // $result = $pdo => query($sql);
+            // $data = $result = fetchAll(FETCH_ASSOC);
+            // return $data;    
+            // Keeping this here just in case the prepared statement modification doesn't work above.
         }
-        catch {
+        catch (PDOExcpetion $e) {
             return null;
         }
     }
