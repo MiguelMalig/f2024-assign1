bfa35a7a5df04ceef6ad24f8fa7645bd18a45bd6
Varainium <rayjustinn@gmail.com>
2024-10-04 17:49:42 -0600

    Changed if statements to switch case, established case statement for passing race as query string.

--- api/drivers.php
+++ api/drivers.php
@@ -3,20 +3,28 @@ include __DIR__ . "data/db_conn.php";
 
 header('Content-Type: application/json');
 
-$driver = getData("select driverRef from Drivers"); // don't know why I put this here, going to keep it just in case I remember why..
+$driver = getData("SELECT driverRef from drivers"); // don't know why I put this here, going to keep it just in case I remember why..
 
-// if driver is passed as a query string..
-if (isset($_GET['driverRef'])) {
-    $driverRef = $_GET['driverRef'];
-    $results = getData("select driverRef from Drivers WHERE driverRef= "+$_GET['driverRef']);
 
+switch () {
+    // if driver is passed as a query string..
+    case isset($_GET['driverRef']):
+        $driverRef = $_GET['driverRef'];
+        $results = getData("SELECT * from drivers WHERE driverRef= ?", $_GET['driverRef']);
+        break;
+
+    // if race is passed as a query string..
+    case isset($_GET['raceID']):
+        $race = $_GET['raceID'];
+        $results = getData("SELECT raceID, driverRef, code, forename, surname, dob, nationality, url from results 
+                            RIGHT JOIN drivers on results.driverId=drivers.driverId 
+                            WHERE results.raceID= ?", $_GET['raceID']);
+        break;
+
+    // if not query string passed, then return all drivers from database..
+    default:
+        $results = getData("SELECT * FROM drivers");
+        break;
 }
-
-// else if not query string passed, then return all drivers from database..
-else {
-    $results = getData("select * from Drivers");
-}
-// TODO: Return drivers for a given race if race is passed as query string..
-
 echo json_encode($results);
 ?>
