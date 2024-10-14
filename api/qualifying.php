bd92df7ccab7a8a524d9969825f94475fe0a9865
Justin Nunez <rayjustinn@gmail.com>
2024-10-14 00:51:09 -0600

    Made qualifying api and results api, also tested that its correctly outputting.

--- api/qualifying.php
+++ api/qualifying.php
@@ -5,5 +5,22 @@
 
     $conn = Database::createConnection();
 
-    
+    if (isset($_GET['raceId'])) {
+        $raceId = $_GET['raceId'];
+        $results = Database::getData("SELECT qualifying.raceId, qualifying.driverId, qualifying.constructorId, drivers.forename, drivers.surname, qualifying.q1, qualifying.q2, qualifying.q3, qualifying.position 
+                                        FROM qualifying
+                                        JOIN drivers ON qualifying.driverId = drivers.driverId
+                                        WHERE qualifying.raceId = ?
+                                        ORDER BY qualifying.position  ASC", $conn, $_GET['raceId']);
+    } 
+    else {
+        // Don't think we need this since instructions doesn't specify to give all of the qualifying table if no query string.
+        //WHEN THERES NO SELECTION YET
+        $results = Database::getData("SELECT qualifying.raceId, qualifying.driverId, qualifying.constructorId, drivers.forename, drivers.surname, qualifying.q1, qualifying.q2, qualifying.q3, qualifying.position 
+                                        FROM qualifying
+                                        JOIN drivers ON qualifying.driverId = drivers.driverId
+                                        ORDER BY qualifying.position  ASC", $conn);
+    }
+echo json_encode($results);
+  //  }
 ?>
--- api/results.php
+++ api/results.php
@@ -5,5 +5,25 @@
 
     $conn = Database::createConnection();
 
-    
+    if (isset($_GET['raceId'])) {
+        $raceId = $_GET['raceId'];
+        $results = Database::getData("SELECT results.raceId, results.driverId, results.constructorId, drivers.forename, drivers.surname, results.position, races.year, races.round, races.date, constructors.name, constructors.constructorRef, constructors.nationality
+                                        FROM results
+                                        JOIN drivers ON results.driverId = drivers.driverId
+                                        JOIN races ON results.raceId = races.raceId
+                                        JOIN constructors ON results.constructorId = constructors.constructorId
+                                        WHERE results.raceId = ?
+                                        ORDER BY results.grid  ASC", $conn, $_GET['raceId']);
+    } 
+    else {
+        // Don't think we need this since instructions doesn't specify to give all of the qualifying table if no query string.
+        //WHEN THERES NO SELECTION YET
+        $results = Database::getData("SELECT results.raceId, results.driverId, results.constructorId, drivers.forename, drivers.surname, results.position, races.year, races.round, races.date, constructors.name, constructors.constructorRef, constructors.nationality
+                                        FROM results
+                                        JOIN drivers ON results.driverId = drivers.driverId
+                                        JOIN races ON results.raceId = races.raceId
+                                        JOIN constructors ON results.constructorId = constructors.constructorId
+                                        ORDER BY results.grid  ASC", $conn);
+    }
+echo json_encode($results);
 ?>
