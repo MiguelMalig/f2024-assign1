6e55a303c0c4b262e51ef3e530fb4a9716438432
Miguel Malig <mmali0761@mtroyal.ca>
2024-10-18 22:55:24 -0600

    Added new photos for index page.. included our github repo as button..

--- index.php
+++ index.php
@@ -18,23 +18,32 @@ require_once 'data/db_conn.php';
     <meta charset="UTF-8">
     <title>F1 Dashboard Project</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css\styles.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
 
 </head>
 <body>
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <!-- NavBAR -->
+    <header class="container-fluid d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
             <span class="fs-4">F1 Dashboard Project</span>
         </a>
-            <ul class="nav nav-pills">
+            <ul class="nav nav-pills mb-4">
                 <li class="nav-item"><a href="#home" class="nav-link active">Home</a>
                 <li class="nav-item"><a href="browse.php" class="nav-link">Browse</a></li>
                 <li class="nav-item"><a href="API's" class="nav-link">APIs</a></li>
             </ul>
+
+           <div class="container-fluid p-4 my-2 bg-light rounded-3 text-white" id="herophoto">
+            <h1 class="display-1 fw-bold mb-5">F1 Racing</h1>
+            <p class="col-md-8 lead mb-5">If everything seems under control, you're just not going fast enough </p>
+            <button class="btn btn-dark btn-lg" type="button"> Browse</button>
+           </div>
+          
     </header>
-<main>
+
+    <!-- Our team info -->
     <div class="container">
         <div class="col-md-4 col-lg-4 px-md-4"> 
             <!-- //ill completely change this layout once we got functions working. -->
